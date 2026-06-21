e8381d62b19141797d656f70fc6aa2e7e07eb64f
Kavindu Kavishka <kavindukavishka56@gmail.com>
2026-06-21 22:32:34 +0530

    portfolio website upload

---
+++ assets/js/main.js
@@ -0,0 +1,83 @@
+/*=============== SHOW & CLOSE MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+/* Show menu */
+if(navToggle){
+   navToggle.addEventListener('click', () =>{
+      navMenu.classList.add('show-menu')
+   })
+}
+
+/* Hide menu */
+if(navClose){
+   navClose.addEventListener('click', () =>{
+      navMenu.classList.remove('show-menu')
+   })
+}
+
+/*=============== REMOVE MOBILE MENU ===============*/
+const navLink = document.querySelectorAll('.nav__link, .nav__contact')
+
+const linkAction = () =>{
+   const navMenu = document.getElementById('nav-menu')
+   // When we click on each nav__link, we remove the show-menu class
+   navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*=============== HOME TEXT CIRCULAR ===============*/
+const homeText = document.getElementById('home-text'),
+      letters = homeText.textContent.trim().split(''), // Coverts text into an array of characters
+      angleStep = 360 / letters.length // Angle for each character; length counts the number of characters
+
+homeText.textContent = '' // Clears the original content
+
+// Iterates through each character
+letters.forEach((char, i) => {
+   const span = document.createElement('span') // Creates a <span> for each letter
+   span.textContent = char // Inserts each character into the span
+   span.style.transform = `rotate(${i * angleStep}deg)` // Rotates each letter based on its index to form the circle
+   homeText.appendChild(span) // Appends the span to the main container
+})
+
+/*=============== HOME TYPED JS ===============*/
+const typeHome = new Typed('#home-typed', {
+   strings: ['Freelancer', 'Web Developer', 'SEO Specialist'], // Insert professions
+   typeSpeed: 60,
+   backSpeed: 30,
+   backDelay: 2000,
+   loop: true,
+})
+
+/*=============== CHANGE HEADER STYLES ===============*/
+const scrollHeader = () =>{
+   const header = document.getElementById('header')
+   this.scrollY >= 50 ? header.classList.add('scroll-header')
+                      : header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+/*=============== SWIPER WORK ===============*/ 
+
+
+/*=============== SERVICES ACCORDION ===============*/ 
+
+
+/*=============== TESTIMONIALS OF DUPLICATE CARDS ===============*/ 
+
+
+/*=============== CONTACT EMAIL JS ===============*/ 
+
+
+/*=============== SHOW SCROLL UP ===============*/ 
+
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+
+/*=============== CUSTOM CURSOR ===============*/
+
+
+/*=============== SCROLLREVEAL ANIMATION ===============*/
